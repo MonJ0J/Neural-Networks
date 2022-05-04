f7cc11a00716f6de60a27f04ed276e6c0dd9929c
MonJ0J <98435698+MonJ0J@users.noreply.github.com>
2022-05-03 23:24:53 -0400

    Update NNW.py

--- NNW.py
+++ NNW.py
@@ -1,10 +1,11 @@
 import numpy as np
-import random 
+import random
+import argparse
 
 def __init__(self):
     np.random.seed(1)
 def sigmoid(x):
-    return 1/(1+np.exp(-x))
+    return 1/(1 + np.exp(-x))
 def sigmoid_deriv(x):
     return sigmoid(x)*(1-sigmoid(x))
 
@@ -45,22 +46,22 @@ def read_file_to_array(file_name):
 
 def forward_propagate(feature_array, parameter_dictionary):
     output_vals = {}
-    
+
     array = parameter_dictionary["weight_hidden"]
     hidden_b = parameter_dictionary["hidden_biases"]
     hidden_layer_values = np.dot(array,feature_array)+hidden_b
     activation  = sigmoid(hidden_layer_values)
     output_vals["hidden_layer_outputs"]=activation
+
     array_2 = parameter_dictionary["weight_output"]
     hidden_o = parameter_dictionary["output_biases"]
     hidden_layer_outputs = np.dot(array_2,feature_array)
     activation_2  = sigmoid(hidden_layer_outputs)
     output_vals["output_layer_outputs"]=activation_2
-    
+
     return output_vals
 
 def find_loss(output_layer_outputs, labels):
-    #print((labels.shape))
     num_examples = labels.shape[1]
     loss = (-1 / num_examples) * np.sum(np.multiply(labels, np.log(output_layer_outputs)) + np.multiply(1-labels, np.log(1-output_layer_outputs)))
     return loss
@@ -95,7 +96,7 @@ def backprop(feature_array, labels, output_vals, weights_biases_dict, verbose=Fa
     # This is our gradient on the bias. It is simply the
     # mean of our errors.
     output_bias_gradient = np.sum(raw_error, axis=1, keepdims=True)/num_examples
- 
+
     if verbose:
         print("output_bias_gradient", output_bias_gradient)
         # We now calculate the amount of error to propagate back to our hidden nodes.
@@ -134,17 +135,16 @@ def backprop(feature_array, labels, output_vals, weights_biases_dict, verbose=Fa
     return gradients
 
 def update_weights_biases(parameter_dictionary, gradients, learning_rate):
-    
+
     hidden_weights = parameter_dictionary["weight_hidden"]
     output_weights = parameter_dictionary["weight_output"]
     hidden_bias = parameter_dictionary["hidden_biases"]
     output_bias = parameter_dictionary["output_biases"]
-    
+
     hidden_weights_gradient = gradients['hidden_weights_gradient']
     hidden_bias_gradient = gradients['hidden_bias_gradient']
     output_weights_gradient = gradients['output_weights_gradient']
     output_bias_gradient = gradients['output_bias_gradient']
-    
     updated_parameters = { "weight_hidden" : hidden_weights - learning_rate*hidden_weights_gradient,
                            "hidden_biases" : hidden_bias - learning_rate*hidden_bias_gradient,
                            "weight_output" : output_weights - learning_rate*output_weights_gradient,
@@ -153,17 +153,32 @@ def update_weights_biases(parameter_dictionary, gradients, learning_rate):
 
 def model_file(file_name, num_inputs, num_hiddens, num_outputs, epochs, learning_rate):
     feature_array, label_array, header_array = read_file_to_array(file_name)
-    print('label array: ', label_array)
     parameter_dictionary = init_weights_biases(num_inputs,num_hiddens,num_outputs)
     epoch = 0
     while epoch <= epochs:
         forward_propgate = forward_propagate(feature_array,parameter_dictionary)
         vals_ForLoss = forward_propgate["output_layer_outputs"]
         loss = find_loss(vals_ForLoss,label_array)
-        print("LOSS",loss)
-        gradients = backprop(feature_array, label_array, forward_propgate, parameter_dictionary)
+        if epoch%100==0:
+            print("LOSS",loss)
+        gradients = backprop(feature_array, label_array, forward_propgate, parameter_dictionary,epoch%100==0)
         parameter_dictionary = update_weights_biases(parameter_dictionary, gradients, learning_rate)
         epoch+=1
     return parameter_dictionary
 
-print(model_file('xor.txt', 2, 2, 1, 100, 0.3))
+# print(model_file('xor.txt', 2, 2, 1, 1000, 0.1))
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='Neural Network')
+    parser.add_argument('-i', '--inputFile', type = str, required = True)
+    parser.add_argument('-n', '--num_inputs', type = int, required = True)
+    parser.add_argument('-p', '--num_hiddens', type = int, required = False)
+    parser.add_argument('-o', '--num_outputs', type = int, required = True)
+    parser.add_argument('-e', '--epochs', type = int, required = True)
+    parser.add_argument('-l', '--learning_rate', type = float, required = True)
+    args = parser.parse_args()
+
+    parameter_dictionary = model_file(args.inputFile, args.num_inputs, args.num_hiddens, args.num_outputs, args.epochs, args.learning_rate)
+    print(parameter_dictionary)
